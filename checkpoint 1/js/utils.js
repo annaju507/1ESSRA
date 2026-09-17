a2d1b08c5d5e67ad1c9aac28ef767abb1acc4315
annajulia_els <annajuliaeliasandrade@gmail.com>
2026-09-16 21:27:30 -0300

    refactor: modulariza validacao e exportacao do formulario

--- checkpoint 1/js/utils.js
+++ checkpoint 1/js/utils.js
@@ -1,4 +1,48 @@
-function gerarArquivoTxt(conteudo, nomeArquivo) {
+// utils.js
+
+function validarCPF(cpf) {
+    cpf = cpf.replace(/\D/g, '');
+
+    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
+        return false;
+    }
+
+    let soma = 0;
+
+    for (let i = 0; i < 9; i++) {
+        soma += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+
+    let resto = (soma * 10) % 11;
+
+    if (resto === 10 || resto === 11) {
+        resto = 0;
+    }
+
+    if (resto !== parseInt(cpf.charAt(9))) {
+        return false;
+    }
+
+    soma = 0;
+
+    for (let i = 0; i < 10; i++) {
+        soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if (resto === 10 || resto === 11) {
+        resto = 0;
+    }
+
+    if (resto !== parseInt(cpf.charAt(10))) {
+        return false;
+    }
+
+    return true;
+}
+
+function salvarDadosEmTXT(conteudo, nomeArquivo) {
     const blob = new Blob([conteudo], {
         type: 'text/plain;charset=utf-8'
     });
